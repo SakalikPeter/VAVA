03e3bb2e2b366e27f19de88c055ff59f3bacf329
Peter Sakalík <sakalikp@gmail.com>
2020-04-10 17:09:29 +0200

    objekty

---
+++ src/main/java/models/Collection.java
@@ -0,0 +1,59 @@
+package models;
+
+import java.sql.Date;
+
+public class Collection {
+    private int id;
+    private int userId;
+    private String name;
+    private Date creationDate;
+    private int size;
+
+    public Collection(int id, int userId, String name, Date creationDate, int size) {
+        this.id = id;
+        this.userId = userId;
+        this.name = name;
+        this.creationDate = creationDate;
+        this.size = size;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getUserId() {
+        return userId;
+    }
+
+    public void setUserId(int userId) {
+        this.userId = userId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Date getCreationDate() {
+        return creationDate;
+    }
+
+    public void setCreationDate(Date creationDate) {
+        this.creationDate = creationDate;
+    }
+
+    public int getSize() {
+        return size;
+    }
+
+    public void setSize(int size) {
+        this.size = size;
+    }
+}
